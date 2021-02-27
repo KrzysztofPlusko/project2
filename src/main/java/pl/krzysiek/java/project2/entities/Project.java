25ed4f27887ec18a2179aa6ffba86568c7d36754
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2021-02-27 14:09:37 +0100

    testowanie encji projektu 2

--- src/main/java/pl/krzysiek/java/project2/entities/Project.java
+++ src/main/java/pl/krzysiek/java/project2/entities/Project.java
@@ -2,5 +2,42 @@ package pl.krzysiek.java.project2.entities;
 
 public class Project {
 
+    private long projectId;
+    private String name;
+    private String stage; // NONSTARTED, INPROGRESS, COMPLETED;
+    private String description;
 
+    public Project() {
+
+    }
+
+    public Project(String name, String stage, String description) {
+        this.name = name;
+        this.stage = stage;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
