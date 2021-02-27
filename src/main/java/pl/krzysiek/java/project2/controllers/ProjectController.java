19f6de17faecd84409e4981a44c744127030ec77
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2021-02-27 19:01:02 +0100

    controller  - save new project to the database

--- src/main/java/pl/krzysiek/java/project2/controllers/ProjectController.java
+++ src/main/java/pl/krzysiek/java/project2/controllers/ProjectController.java
@@ -1,15 +1,20 @@
 package pl.krzysiek.java.project2.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.krzysiek.java.project2.dto.IProjectRepository;
 import pl.krzysiek.java.project2.entities.Project;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+
+    @Autowired
+    IProjectRepository proRepo;
 
     @GetMapping("/new")
     public String diplayProjectForm(Model model) {
@@ -21,7 +26,10 @@ public class ProjectController {
 
     @PostMapping("/save")
     public String createProject(Project project, Model model) {
+
+        proRepo.save(project);
+
         //
-        return "";
+        return "redirect:/projects/new";
     }
 }
