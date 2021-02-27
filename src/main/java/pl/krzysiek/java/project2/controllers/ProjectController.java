96ac5c48509daa1b52c30c446ee55d968230da22
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2021-02-27 17:47:36 +0100

    adding ProjectController 2 "/projects/new"

--- src/main/java/pl/krzysiek/java/project2/controllers/ProjectController.java
+++ src/main/java/pl/krzysiek/java/project2/controllers/ProjectController.java
@@ -1,4 +1,14 @@
 package pl.krzysiek.java.project2.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/projects")
 public class ProjectController {
+
+    @RequestMapping("/new")
+    public String diplayProjectForm() {
+        return "project-new";
+    }
 }
