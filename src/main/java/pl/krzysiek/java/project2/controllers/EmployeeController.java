5eadaa756262922095f245f736f82088d69f55e9
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2021-02-28 08:13:35 +0100

    employee controller 4

--- src/main/java/pl/krzysiek/java/project2/controllers/EmployeeController.java
+++ src/main/java/pl/krzysiek/java/project2/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.krzysiek.java.project2.dto.IEmployeeRepository;
 import pl.krzysiek.java.project2.entities.Employee;
@@ -23,7 +24,7 @@ public class EmployeeController {
         return "new-employee";
     }
 
-    @GetMapping("/save")
+    @PostMapping("/save")
     public String createEmployee(Employee employee, Model model) {
 
         empRepo.save(employee);
