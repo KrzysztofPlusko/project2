ed6e51939216fe922abf87f3dc6237b7885aa30e
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2021-02-28 14:51:43 +0100

    adding current  employees display

--- src/main/java/pl/krzysiek/java/project2/controllers/HomeController.java
+++ src/main/java/pl/krzysiek/java/project2/controllers/HomeController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.krzysiek.java.project2.dto.IEmployeeRepository;
 import pl.krzysiek.java.project2.dto.IProjectRepository;
+import pl.krzysiek.java.project2.entities.Employee;
 import pl.krzysiek.java.project2.entities.Project;
 
 import java.util.List;
@@ -15,12 +17,17 @@ public class HomeController {
     @Autowired
     IProjectRepository proRepo;
 
+    @Autowired
+    IEmployeeRepository empRepo;
+
 
     @GetMapping("/")
     public String displayHome(Model model) {
 
         List<Project> projects =  proRepo.findAll();
         model.addAttribute("projectsList",projects);
+        List<Employee> employees = empRepo.findAll();
+        model.addAttribute("employeesList",employees);
         return "home";
     }
 }
--- src/main/java/pl/krzysiek/java/project2/dto/IEmployeeRepository.java
+++ src/main/java/pl/krzysiek/java/project2/dto/IEmployeeRepository.java
@@ -2,6 +2,12 @@ package pl.krzysiek.java.project2.dto;
 
 import org.springframework.data.repository.CrudRepository;
 import pl.krzysiek.java.project2.entities.Employee;
+import pl.krzysiek.java.project2.entities.Project;
+
+import java.util.List;
 
 public interface IEmployeeRepository extends CrudRepository<Employee, Long> {
+
+    @Override
+    public List<Employee> findAll();
 }
