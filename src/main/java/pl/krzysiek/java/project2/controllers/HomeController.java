a5bb72f9d3f59c98dc683c381d66ae6f94affd7f
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2021-02-28 09:41:50 +0100

    IProjectRepository overriding method findAll()

--- src/main/java/pl/krzysiek/java/project2/controllers/HomeController.java
+++ src/main/java/pl/krzysiek/java/project2/controllers/HomeController.java
@@ -1,4 +1,22 @@
 package pl.krzysiek.java.project2.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.krzysiek.java.project2.dto.IProjectRepository;
+
+@Controller
 public class HomeController {
+
+    @Autowired
+    IProjectRepository proRepo;
+
+
+    @GetMapping("/")
+    public String displayHome(Model model) {
+
+
+        return "home";
+    }
 }
--- src/main/java/pl/krzysiek/java/project2/dto/IProjectRepository.java
+++ src/main/java/pl/krzysiek/java/project2/dto/IProjectRepository.java
@@ -3,5 +3,10 @@ package pl.krzysiek.java.project2.dto;
 import org.springframework.data.repository.CrudRepository;
 import pl.krzysiek.java.project2.entities.Project;
 
+import java.util.List;
+
 public interface IProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    public List<Project> findAll();
 }
